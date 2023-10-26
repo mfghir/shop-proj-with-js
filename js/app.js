b5dea2814c77865c9c47fb650a4284196f8035ed
mfghir <fatemeghafari77@gmail.com>
2023-10-26 01:08:02 -0700

    change btn style

--- js/app.js
+++ js/app.js
@@ -2,24 +2,45 @@ const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
 const buttons = document.querySelectorAll(".filter");
 
+const changeClass = () => {
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filter) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
+};
+
 const searchHandler = (e) => {
   const searchValue = e.target.value.toLowerCase().trim();
   products.forEach((product) => {
     const productName = product.children[1].innerText.toLowerCase();
     if (productName.includes(searchValue)) {
-        product.style.display = "block";
-    }else{
-        product.style.display = "none";
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
     }
-
   });
 };
 
-const filterHandler=(e)=>{
-  const filter = e.target.dataset.filter
-}
+const filterHandler = (e) => {
+  const filter = e.target.dataset.filter;
+  changeClass(filter);
 
-searchInput.addEventListener("keyup",searchHandler)
-buttons.forEach(buttons=>{
-  buttons.addEventListener('click',filterHandler)
-})
+  products.forEach((product) => {
+    const category = product.dataset.category;
+    if (filter === "all") {
+      product.style.display = "block";
+    } else {
+      filter === category
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
+
+searchInput.addEventListener("keyup", searchHandler);
+buttons.forEach((buttons) => {
+  buttons.addEventListener("click", filterHandler);
+});
